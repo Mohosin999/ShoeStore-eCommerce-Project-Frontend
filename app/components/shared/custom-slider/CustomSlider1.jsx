146d8f67d131e3d8e8f812d57d531dc19242138d
Mohosin999 <mohosin.h.akash@gmail.com>
2023-09-15 08:06:17 +0600

    slider card design

--- app/components/shared/custom-slider/CustomSlider1.jsx
+++ app/components/shared/custom-slider/CustomSlider1.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Link from "next/link";
 import { Navigation, Pagination, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,7 +10,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Image from "next/image";
 import NavSlideBtn from "./SlideNavButton";
-import Link from "next/link";
+import Button from "../../UI/button/Button";
 
 // External Data Import
 const url = "http://127.0.0.1:1337/api/products/hot-discount?populate=*";
@@ -32,10 +33,7 @@ const CustomSlider1 = () => {
   const [hoveredProduct, setHoveredProduct] = useState(null);
 
   return (
-    <div className="mt-[5rem] px-10">
-      <h1>Hot Discount!</h1>
-      <br />
-
+    <div class="mt-[5rem] px-10">
       <Swiper
         modules={[Navigation, Pagination, A11y]}
         slidesPerView={1}
@@ -50,7 +48,7 @@ const CustomSlider1 = () => {
           products?.filteredData.map((item) => (
             <SwiperSlide key={item.id}>
               <div
-                className="max-w-sm rounded overflow-hidden shadow-lg relative border border-gray-700 m-2"
+                class="max-w-sm rounded overflow-hidden shadow-lg shadow-gray-800 relative border border-gray-700 m-2"
                 onMouseEnter={() => setHoveredProduct(item.id)}
                 onMouseLeave={() => setHoveredProduct(null)}
               >
@@ -61,24 +59,34 @@ const CustomSlider1 = () => {
                   alt={item.name}
                 />
 
+                <div class="bg-gray-800 p-4 text-center">
+                  <h2 class="text-xl text-gray-200 font-semibold mb-2">
+                    {item.attributes.name}
+                  </h2>
+                  <p class="text-lg text-red-400">
+                    Original Price:{" "}
+                    <span class="font-bold pl-3">
+                      ${item.attributes.original_price}
+                    </span>
+                  </p>
+                  <p class="text-lg text-green-500">
+                    Discounted Price:{" "}
+                    <span class="font-bold pl-3">
+                      ${item.attributes.discounted_price}
+                    </span>
+                  </p>
+                </div>
+
                 {hoveredProduct === item.id && (
-                  <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-60 text-white text-center backdrop-blur-sm">
-                    <h2 className="text-xl font-semibold mb-2">
-                      {item.attributes.name}
-                    </h2>
-                    <p className="text-lg">
-                      Original Price: ${item.attributes.original_price}
-                    </p>
-                    <p className="text-lg">
-                      Discounted Price: ${item.attributes.discounted_price}
-                    </p>
-                    <Link href="/welcome">Buy Now</Link>
+                  <div class="absolute inset-0 flex flex-col justify-center items-center text-white text-center backdrop-blur-sm">
+                    <div class="text-center">
+                      <Button href="/" label="Buy Now" />
+                    </div>
                   </div>
                 )}
               </div>
             </SwiperSlide>
           ))}
-
         <NavSlideBtn />
       </Swiper>
     </div>
@@ -86,89 +94,3 @@ const CustomSlider1 = () => {
 };
 
 export default CustomSlider1;
-
-// "use client";
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { Navigation, Pagination, A11y } from "swiper/modules";
-// import { Swiper, SwiperSlide } from "swiper/react";
-
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import Image from "next/image";
-// import NavSlideBtn from "./SlideNavButton";
-// import Link from "next/link";
-
-// // External Data Import
-// const url = "http://127.0.0.1:1337/api/products/hot-discount?populate=*";
-
-// const CustomSlider1 = () => {
-//   const [products, setProducts] = useState([]);
-//   const [isHovered, setIsHovered] = useState(false);
-
-//   const getProductsFromBackend = async () => {
-//     const fetchData = await axios.get(url);
-//     const products = fetchData.data;
-
-//     setProducts(products);
-//   };
-
-//   useEffect(() => {
-//     getProductsFromBackend();
-//   }, []);
-
-//   // const fetchData = await axios.get(url);
-//   // const products = fetchData.data;
-
-//   return (
-//     <div class="mt-[5rem] px-10">
-//       <h1>Hot Discount!</h1>
-//       <br />
-
-//       <Swiper
-//         modules={[Navigation, Pagination, A11y]}
-//         slidesPerView={1}
-//         spaceBetween={15}
-//         breakpoints={{
-//           480: { slidesPerView: 2 },
-//           780: { slidesPerView: 3 },
-//           1280: { slidesPerView: 3 },
-//         }}
-//       >
-//         {products?.filteredData &&
-//           products?.filteredData.map((item) => (
-//             <SwiperSlide
-//               key={item.id}
-//               // className="bg-red-200 !flex justify-center items-center "
-//             >
-//               <div class="max-w-sm rounded overflow-hidden shadow-lg relative border border-gray-700 m-2">
-//                 <Image
-//                   src={item.attributes.thumbnail.data.attributes.url}
-//                   width={400}
-//                   height={400}
-//                   alt={item.name}
-//                   // class="hover:bg-green-800 hover:opacity-20"
-//                   onMouseEnter={() => setIsHovered(true)}
-//                   onMouseLeave={() => setIsHovered(false)}
-//                 />
-
-//                 {isHovered && (
-//                   <div class="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-60 text-white text-center backdrop-blur-sm">
-//                     <h2 class="text-xl font-semibold mb-2">{item.name}</h2>
-//                     <p class="text-lg">${item.original_price}</p>
-//                     <p class="text-lg">${item.discounted_price}</p>
-//                     <Link href="/welcome">Buy Now</Link>
-//                   </div>
-//                 )}
-//               </div>
-//             </SwiperSlide>
-//           ))}
-
-//         <NavSlideBtn />
-//       </Swiper>
-//     </div>
-//   );
-// };
-
-// export default CustomSlider1;
