0ae2bea245274eb09f027f6c900cf59b917bf5aa
Mohosin999 <mohosin.h.akash@gmail.com>
2023-09-16 11:29:33 +0600

    reusable ui title component

---
+++ app/components/UI/title/Title.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * A reusable component for rendering a title.
+ *
+ * @param {string} title - The title to be displayed.
+ * @param {string} className - You can give your own style by it.
+ * @returns {JSX.Element} - Returns a component to display title.
+ */
+const Title = ({ title, className }) => {
+  return (
+    <div
+      class={`text-gray-200 text-3xl text-orange-400 font-bold mt-20 mb-6 ${
+        className || ""
+      }`}
+    >
+      {title}
+    </div>
+  );
+};
+
+// Define PropTypes for the component
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+export default Title;
